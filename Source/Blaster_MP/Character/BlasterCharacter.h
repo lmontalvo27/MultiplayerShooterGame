aee976f77c2385f01e2e432f28bf33ad78d1fe89
lmontalvo27 <lmontalvo1998@gmail.com>
2024-01-26 10:22:38 -0800

    More code to BlasterCharacter

--- Source/Blaster_MP/Character/BlasterCharacter.h
+++ Source/Blaster_MP/Character/BlasterCharacter.h
@@ -14,16 +14,19 @@ class BLASTER_MP_API ABlasterCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ABlasterCharacter();
-
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:	
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
+
+public:
 
 };
