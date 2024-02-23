f27aa10fb7188b27dba609e454df1da5e1d6dc1d
lmontalvo27 <lmontalvo1998@gmail.com>
2024-02-23 10:01:37 -0800

    Play Montage on Server and Clients

--- Source/Blaster_MP/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster_MP/BlasterComponents/CombatComponent.cpp
@@ -17,6 +17,14 @@ UCombatComponent::UCombatComponent()
 	AimWalkSpeed = 450;
 }
 
+void UCombatComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UCombatComponent, EquippedWeapon);
+	DOREPLIFETIME(UCombatComponent, bAiming);
+
+}
 
 void UCombatComponent::BeginPlay()
 {
@@ -60,9 +68,23 @@ void UCombatComponent::OnRep_EquippedWeapon()
 void UCombatComponent::FireButtonPressed(bool bPressed)
 {
 	bFireButtonPressed = bPressed;
+	
+	if (bFireButtonPressed)
+	{
+		ServerFire();
+	}
+}
 
+void UCombatComponent::ServerFire_Implementation()
+{
+	MulticastFire();
+}
+
+// Multicast called on Server would show on Server and all clients
+void UCombatComponent::MulticastFire_Implementation()
+{
 	if (EquippedWeapon == nullptr) return;
-	if (Character && bFireButtonPressed)
+	if (Character)
 	{
 		Character->PlayFireMontage(bAiming);
 		EquippedWeapon->Fire();
@@ -70,19 +92,14 @@ void UCombatComponent::FireButtonPressed(bool bPressed)
 }
 
 
+
+
 void UCombatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
-void UCombatComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	
-	DOREPLIFETIME(UCombatComponent, EquippedWeapon);
-	DOREPLIFETIME(UCombatComponent, bAiming);
 
-}
 
 void UCombatComponent::EquipWeapon(AWeapon* WeaponToEquip)
 {
